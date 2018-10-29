06fc67e1e1c30842adbda14a88ebd65e9fa1acf6
Krishna Sri Somepalli <krishnasri.somepalli@outlook.com>
2018-10-29 10:31:40 +0530

    Forward & Reverse Engineering Done.

---
+++ Forward & Reverse Engineering/User (Forward Engineering output).java
@@ -0,0 +1,81 @@
+
+
+/**
+ * Class User
+ */
+public class User {
+
+  //
+  // Fields
+  //
+
+  private String name;
+  private String userID;
+  
+  //
+  // Constructors
+  //
+  public User () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (String newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private String getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of userID
+   * @param newVar the new value of userID
+   */
+  private void setUserID (String newVar) {
+    userID = newVar;
+  }
+
+  /**
+   * Get the value of userID
+   * @return the value of userID
+   */
+  private String getUserID () {
+    return userID;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       String
+   */
+  public String getInfo()
+  {
+  }
+
+
+  /**
+   * @return       String
+   */
+  public String greeting()
+  {
+  }
+
+
+}
